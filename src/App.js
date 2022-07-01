142cc7c608adb6fefb7183290b2494879124af45
Ava <avamsteimle@gmail.com>
2022-07-01 12:05:46 -0700

    holy grail custom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Nav from './components/Nav.js';
+import Main from './components/Main.js';
+import Footer from './components/Footer.js';
+import Aside from './components/Aside.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="page-wrap">
+  <header className="page-header">
+    <Nav />
+    Header
+  </header>
+  <main className="page-main">
+    <Main />
+    Main
+  </main>
+  <aside className="page-sidebar">
+    <Aside />
+    Aside
+  </aside>
+  <footer className="page-footer">
+    <Footer />
+    Footer
+  </footer>
+</div>
   );
 }
 
